c5d5ac648a1921a3d443f24d933c3b43f4687747
PabloC <cerezotrabajo7@correo.ugr.es>
2021-05-26 00:51:21 +0200

    Selección de personajes básica y cambio de escena

---
+++ Assets/scripts/SeleccionPersonaje/BotonPersonaje.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonPersonaje : MonoBehaviour
+{
+    private int numero;
+    private bool pulsado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pulsado = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        pulsado = true;
+        switch (numero)
+        {
+            case 0:
+                print("Hero Knight");
+                break;
+            case 1:
+                print("Medieval Warrior");
+                break;
+            case 2:
+                print("Dragon Warrior");
+                break;
+        }
+    }
+
+    public int getNumero()
+    {
+        return numero;
+    }
+
+    public void setNumero(int numero)
+    {
+        this.numero = numero;
+    }
+
+    public bool getPulsado()
+    {
+        return pulsado;
+    }
+}
---
+++ Assets/scripts/SeleccionPersonaje/SeleccionSystem.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SeleccionSystem : MonoBehaviour
+{
+    //private int personaje1, personaje2;
+    private int jugadorEligiendo;
+    private bool terminado_j1, terminado_j2, escogidos;
+    private String nombre;
+    private GameObject[] listaPersonajes;
+
+    public GameObject botonJugar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugadorEligiendo = 0; // Primero escoge el jugador 1
+        terminado_j1 = terminado_j2 = escogidos = false;
+        //personaje1 = personaje2 = 0;
+        listaPersonajes = new GameObject[3];
+
+        for (int i = 0; i < listaPersonajes.Length; i++)
+        {
+            listaPersonajes[i] = GameObject.Find("Personaje" + i);
+            listaPersonajes[i].GetComponent<BotonPersonaje>().setNumero(i);
+            listaPersonajes[i].SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Detectamos si hemos seleccionado un personaje
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Ratón pulsado");
+
+            if (EventSystem.current.currentSelectedGameObject == botonJugar)
+            {
+                if (escogidos)
+                {
+                    // Esperamos a que todos los jugadores escojan su personaje
+                    print("Los jugadores han escogido");
+                    // Cargar escena de combate
+                    SceneManager.LoadScene("EscenaCombate");
+                }
+            }
+            else
+            {
+                // Se recorre la lista preguntando quién ha sido seleccionado
+                for (int i = 0; i < listaPersonajes.Length; i++)
+                {
+                    if (listaPersonajes[i].GetComponent<BotonPersonaje>().getPulsado())
+                    {
+                        nombre = listaPersonajes[i].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>()
+                            .text;
+
+                        // Si no ha seleccionado aún el j1, se lo asignamos
+                        if (!terminado_j1)
+                        {
+                            GameObject.Find("SeleccionJ1").transform.GetChild(0).gameObject
+                                .GetComponent<TextMeshProUGUI>().SetText(nombre);
+                            terminado_j1 = true;
+                        }
+                        // Falta el jugador 2 por asignar
+                        else
+                        {
+                            GameObject.Find("SeleccionJ2").transform.GetChild(0).gameObject
+                                .GetComponent<TextMeshProUGUI>().SetText(nombre);
+                            terminado_j2 = true;
+                        }
+                    }
+                }
+            }
+
+            if (terminado_j1 && terminado_j2) escogidos = true;
+        }
+    }
+}
